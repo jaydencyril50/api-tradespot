b9ef7a025834b716b54f14e910b9072f8abc578a
Jayden Cyril <jaydencyril50@gmail.com>
2025-07-10 06:59:23 -0700

    a

--- routes/flexDropLink.ts
+++ routes/flexDropLink.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 const FlexDropLink = require('../models/FlexDropLink');
 const User = require('../models/User');
 const { v4: uuidv4 } = require('uuid');
-const authenticateToken = require('../middleware/authenticateToken');
+const authenticateToken = require('../middleware/authenticateToken').default;
 
 // Admin creates a flex drop link
 router.post('/create', authenticateToken, async (req, res) => {
